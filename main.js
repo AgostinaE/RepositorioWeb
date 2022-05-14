3cdb49ee0c90fdc48205125ab7839e8e0f7981ee
Joan Sebastian Fajardo Perico <67761930+JSFP@users.noreply.github.com>
2022-05-14 17:58:20 -0500

    Commits avances pagina CET
    Olle disculpa la demora estaba finiquitando detalles ya para empezar con el desarrollo concreto y tuve un percance con los estilos jajaja se me borraron de momento y tuve que volverlos a escribir pero bueno ya mañana estara index listo y ya seguir en esta semana poder acabar confiando en Dios

---
+++ main.js
@@ -0,0 +1,39 @@
+//esta funcion permite cambiar la clase del elemento con el id topMenu y transformarla en la clase responsive 
+function myFunction() {
+    x = document.getElementById("menu-responsive");
+    if (x.className === "menu") {
+      x.className += "responsive";
+    } else {
+      x.className = "menu";
+    }
+    
+  } 
+
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); // cambia cada 10 segundos
+} 
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
